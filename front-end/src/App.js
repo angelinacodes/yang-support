0eeb54323039a8fec35e0d78ce4d5830a3bdc049
angelinamay <angelinacodes@gmail.com>
2019-11-03 15:36:15 -0800

    set up react-router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,42 +1,62 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // import NavBar from "./components/menu/menu.js";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 import NavBar from "components/menu/menu.js";
+import InteractiveContainer from "components/interactive/interactive-container.js";
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: "#133467",
-      main: "#133467",
-      dark: "#133467",
-      contrastText: "#FFFFFF"
-    },
-    secondary: {
-      light: "#DA3248",
-      main: "#DA3248",
-      dark: "#DA3248",
-      contrastText: "#FFFFFF"
-    },
-    // error: {
-    //   light: palette.error[300],
-    //   main: palette.error[500],
-    //   dark: palette.error[700],
-    //   contrastText: getContrastText(palette.error[500])
-    // },
-    textColor: "#13294B",
-    // primary1Color: Colors.white,
-    // primary2Color: Colors.indigo700,
-    // accent1Color: Colors.redA200,
-    // pickerHeaderColor: Colors.darkBlack,
-    alternateTextColor: "#133467"
-  }
+	palette: {
+		primary: {
+			light: "#133467",
+			main: "#133467",
+			dark: "#133467",
+			contrastText: "#FFFFFF"
+		},
+		secondary: {
+			light: "#DA3248",
+			main: "#DA3248",
+			dark: "#DA3248",
+			contrastText: "#FFFFFF"
+		},
+		// error: {
+		//   light: palette.error[300],
+		//   main: palette.error[500],
+		//   dark: palette.error[700],
+		//   contrastText: getContrastText(palette.error[500])
+		// },
+		textColor: "#13294B",
+		// primary1Color: Colors.white,
+		// primary2Color: Colors.indigo700,
+		// accent1Color: Colors.redA200,
+		// pickerHeaderColor: Colors.darkBlack,
+		alternateTextColor: "#133467"
+	}
 });
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <NavBar />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Router>
+				<NavBar />
+
+				<Switch>
+					<Route exact path="/">
+						<StubComponent />
+					</Route>
+					<Route path="/interactive">
+						<InteractiveContainer />
+					</Route>
+					<Route path="/policies">
+						<StubComponent />
+					</Route>
+				</Switch>
+			</Router>
+		</ThemeProvider>
+	);
 }
+
+const StubComponent = () => {
+	return <div></div>;
+};
--- front-end/src/components/menu/menu.js
+++ front-end/src/components/menu/menu.js
@@ -1,147 +1,147 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { fade, makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
-import Badge from "@material-ui/core/Badge";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import MailIcon from "@material-ui/icons/Mail";
-import NotificationsIcon from "@material-ui/icons/Notifications";
-import MoreIcon from "@material-ui/icons/MoreVert";
 
 import css from "./menu.module.css";
 
 const useStyles = makeStyles(theme => ({
-  grow: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block"
-    }
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(3),
-      width: "auto"
-    }
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  inputRoot: {
-    color: "inherit"
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: 200
-    }
-  },
-  sectionDesktop: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex"
-    }
-  },
-  sectionMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none"
-    }
-  }
+	grow: {
+		flexGrow: 1
+	},
+	menuButton: {
+		marginRight: theme.spacing(2)
+	},
+	title: {
+		display: "none",
+		[theme.breakpoints.up("sm")]: {
+			display: "block"
+		}
+	},
+	search: {
+		position: "relative",
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		"&:hover": {
+			backgroundColor: fade(theme.palette.common.white, 0.25)
+		},
+		marginRight: theme.spacing(2),
+		marginLeft: 0,
+		width: "100%",
+		[theme.breakpoints.up("sm")]: {
+			marginLeft: theme.spacing(3),
+			width: "auto"
+		}
+	},
+	searchIcon: {
+		width: theme.spacing(7),
+		height: "100%",
+		position: "absolute",
+		pointerEvents: "none",
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	inputRoot: {
+		color: "inherit"
+	},
+	inputInput: {
+		padding: theme.spacing(1, 1, 1, 7),
+		transition: theme.transitions.create("width"),
+		width: "100%",
+		[theme.breakpoints.up("md")]: {
+			width: 200
+		}
+	},
+	sectionDesktop: {
+		display: "none",
+		[theme.breakpoints.up("md")]: {
+			display: "flex"
+		}
+	},
+	sectionMobile: {
+		display: "flex",
+		[theme.breakpoints.up("md")]: {
+			display: "none"
+		}
+	}
 }));
 
 export default function PrimarySearchAppBar() {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
+	const classes = useStyles();
+	const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const open = Boolean(anchorEl);
+	const open = Boolean(anchorEl);
 
-  const handleMenuOpen = event => {
-    setAnchorEl(event.currentTarget);
-  };
+	const handleMenuOpen = event => {
+		setAnchorEl(event.currentTarget);
+	};
 
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
+	const handleMenuClose = () => {
+		setAnchorEl(null);
+	};
 
-  return (
-    <div className={css.container}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleMenuOpen}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="long-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={open}
-            onClose={handleMenuClose}
-            // PaperProps={{
-            //   style: {
-            //     maxHeight: ITEM_HEIGHT * 4.5,
-            //     width: 200
-            //   }
-            // }}
-          >
-            <MenuItem>Interactive</MenuItem>
-            <MenuItem>Policies</MenuItem>
-          </Menu>
+	return (
+		<div className={css.container}>
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton
+						edge="start"
+						className={classes.menuButton}
+						color="inherit"
+						aria-label="open drawer"
+						onClick={handleMenuOpen}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Menu
+						id="long-menu"
+						anchorEl={anchorEl}
+						keepMounted
+						open={open}
+						onClose={handleMenuClose}
+						// PaperProps={{
+						//   style: {
+						//     maxHeight: ITEM_HEIGHT * 4.5,
+						//     width: 200
+						//   }
+						// }}
+					>
+						<MenuItem onClick={handleMenuClose}>
+							<Link to="/interactive">Interactive</Link>
+						</MenuItem>
+						<MenuItem onClick={handleMenuClose}>
+							<Link to="/policies">Policies</Link>
+						</MenuItem>
+					</Menu>
 
-          <Typography className={classes.title} variant="h6" noWrap>
-            YANG SUPPORT
-          </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput
-              }}
-              inputProps={{ "aria-label": "search" }}
-            />
-          </div>
-          <div className={classes.grow} />
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+					<Typography className={classes.title} variant="h6" noWrap>
+						YANG SUPPORT
+					</Typography>
+					<div className={classes.search}>
+						<div className={classes.searchIcon}>
+							<SearchIcon />
+						</div>
+						<InputBase
+							placeholder="Search…"
+							classes={{
+								root: classes.inputRoot,
+								input: classes.inputInput
+							}}
+							inputProps={{ "aria-label": "search" }}
+						/>
+					</div>
+					<div className={classes.grow} />
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
 }
